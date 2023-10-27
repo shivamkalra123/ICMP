a439f26db004c8b667c70797fe2c158b2f8ea323
Shivam <kalrashivam47@gmail.com>
2023-10-27 21:28:45 +0530

    graduation

--- src/Student_centre/graduation.js
+++ src/Student_centre/graduation.js
@@ -21,38 +21,66 @@ const graduation = () => {
           <li>Students are reminded that regulations apply as listed under the Bachelor of Science, Performance Review and Continuation. Choice of option and specialization will be limited if good academic standing is not maintained or if academically more qualified students fill limited spaces.</li>
           <li>A major, combined major, double major, or General Science option requires a minimum of 120 credits but may require more. A dual degree will require more than 120 credits but some combinations may be completed in less than the minimum of 180 credits required to complete both a first and second degree sequentially. An honours or combined honours option requires a minimum of 132 credits, but may require more. The B.Sc. degree must be obtained within a maximum of 180 attempted credits including credits transferred to the degree.</li>
         </ul>
-
+<hr></hr>
+<h1>Major (0376): Computer Science(CPSC) </h1>
+<h4>Second Year</h4>
         <table style={{ marginTop: '40px' }}>
-          <thead>
-            <tr>
-              <th>Subject</th>
-              <th>Required Credits</th>
-            </tr>
-          </thead>
+        
           <tbody>
             <tr>
-              <td>English</td>
+              <td>CPSC 210</td>
               <td>6</td>
             </tr>
             <tr>
-              <td>Mathematics</td>
+              <td>CPSC 213 221</td>
               <td>6</td>
             </tr>
             <tr>
-              <td>Science</td>
+              <td>MATH 200 221</td>
               <td>6</td>
             </tr>
             <tr>
-              <td>History</td>
+              <td>STAT 241 or 251 </td>
               <td>6</td>
             </tr>
             <tr>
-              <td>Arts</td>
+              <td>Electives</td>
               <td>6</td>
             </tr>
             <tr>
-              <td>Foreign Language</td>
+              <th>Total Credits</th>
+              <th>30</th>
+            </tr>
+          </tbody>
+        </table>
+        <hr></hr>
+        <h4>Third Year</h4>
+        <table style={{ marginTop: '40px' }}>
+         
+          <tbody>
+            <tr>
+              <td>CPSC 310,313,320</td>
               <td>6</td>
+            </tr>
+            <tr>
+              <td>CPSC courses numbered 300 or higher</td>
+              <td>6</td>
+            </tr>
+            <tr>
+              <td>CPSC courses numbered 400 or higher</td>
+              <td>6</td>
+            </tr>
+            <tr>
+              <td>Electives </td>
+              <td>32</td>
+            </tr>
+            <tr>
+              <td>Total Credits</td>
+              <td>42</td>
+            </tr>
+            <tr>
+              <th><i>Total Credits for degree</i></th>
+              <th>120</th>
             </tr>
           </tbody>
         </table>
