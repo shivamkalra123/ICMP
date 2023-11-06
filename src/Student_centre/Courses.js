01a67f048e940b9ea50e0bc0e1de7d6a5b665803
Shivam <kalrashivam47@gmail.com>
2023-11-06 11:19:01 +0530

    Course-Page

--- src/Student_centre/Courses.js
+++ src/Student_centre/Courses.js
@@ -1,44 +1,149 @@
+// CoursePage.js
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import CourseTable from '../Student_centre/courseTable';
+import '../Student_centre/admin_css/courses.css';
 import ParentComponent from './admin_nav';
 
-const CoursesPage = () => {
-  const courses = [
-    { name: 'Course A', creditPoints: 3 },
-    { name: 'Course B', creditPoints: 4 },
-    // Add more courses as needed
-  ];
+const SubCourseList = ({ subCourses, handleCheckboxChange }) => {
+  return (
+    <ul className="sub-course-list">
+    {subCourses.map(subCourse => (
+      <li key={subCourse.id}>
+        <input 
+          type="checkbox" 
+          checked={subCourse.checked}
+          onChange={() => handleCheckboxChange(subCourse)}
+        />
+         <a href="#">{subCourse.name}</a>
+          <p>{subCourse.description}</p>
+      
+      </li>
+    ))}
+  </ul>
+  );
+};
 
-  const [selectedCourses, setSelectedCourses] = useState([]);
+const CourseList = ({ courses, handleCourseClick, handleCheckboxChange }) => {
+  return (
+    <div className="course-list-container">
+      <table className="course-list-table">
+        <thead>
+          <tr>
+            <th>Course</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map(course => (
+            <tr key={course.id}>
+              <td>
+                <a href="#" onClick={(e) => handleCourseClick(e, course)}>{course.name}</a>
+                {course.expanded && (
+            <SubCourseList subCourses={course.subCourses} handleCheckboxChange={handleCheckboxChange} />
+          )}
+              </td>
+              <td>{course.title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+     
+    </div>
+  );
+};
+
+
+const CoursePage = () => {
+  const [courses, setCourses] = useState([
+    {
+      id: 1,
+      name: 'CS50',
+      title: 'Introduction to Computer Science',
+      expanded: false,
+      subCourses: [
+        { id: 11, name: 'Fundamentals of Programming', title: 'CS50x', description: 'Introduction to the intellectual enterprises of computer science and the art of programming.' , checked: false},
+        { id: 12, name: 'Web Development', title: 'CS50 Web', description: 'Learn to design and develop dynamic, secure, and high-performance web applications.', checked: false },
+      ]
+    },
+    {
+      id: 2,
+      name: 'ECON 1010',
+      title: 'Principles of Economics',
+      expanded: false,
+      subCourses: [
+        { id: 21, name: 'Microeconomics', title: 'ECON 1011', description: 'Introduction to microeconomic theory and policy analysis.', checked: false },
+        { id: 22, name: 'Macroeconomics', title: 'ECON 1012', description: 'Introduction to macroeconomic theory and policy analysis.', checked: false },
+      ]
+    },
+    {
+      id: 3,
+      name: 'PSYCH 1',
+      title: 'Introduction to Psychology',
+      expanded: false,
+      subCourses: [
+        { id: 31, name: 'Social Psychology', title: 'PSYCH 15', description: 'Introduction to the systematic study of human behavior in social contexts.', checked: false },
+        { id: 32, name: 'Abnormal Psychology', title: 'PSYCH 16', description: 'Introduction to the understanding and treatment of psychological disorders.', checked: false },
+      ]
+    },
+    {
+      id: 4,
+      name: 'PHIL 1',
+      title: 'Introduction to Philosophy',
+      expanded: false,
+      subCourses: [
+        { id: 41, name: 'Ethics', title: 'PHIL 21', description: 'Introduction to fundamental problems in moral philosophy.', checked: false },
+        { id: 42, name: 'Metaphysics', title: 'PHIL 22', description: 'Introduction to the study of reality, existence, and identity.', checked: false },
+      ]
+    }
+  ]);
+
   const history = useHistory();
 
-  const handleCheckboxChange = (course) => {
-    setSelectedCourses(prevSelected => {
-      const updatedSelected = prevSelected.includes(course)
-        ? prevSelected.filter(c => c !== course)
-        : [...prevSelected, course];
-      return updatedSelected;
+  const handleCourseClick = (e, clickedCourse) => {
+    e.preventDefault();
+    setCourses(prevCourses => prevCourses.map(course => ({
+      ...course,
+      expanded: course.id === clickedCourse.id ? !course.expanded : false
+    })));
+  };
+
+  const handleCheckboxChange = (subCourse) => {
+    setCourses(prevCourses => prevCourses.map(course => ({
+      ...course,
+      subCourses: course.subCourses.map(sc => 
+        sc.id === subCourse.id ? { ...sc, checked: !sc.checked } : sc
+      )
+    })));
+  };
+
+  const handleSubCourseClick = () => {
+    const selectedSubCourses = courses.flatMap(course => 
+      course.subCourses.filter(subCourse => subCourse.checked)
+    );
+    const data = {
+      selectedSubCourses
+    };
+    history.push({
+      pathname: '/addCourse',
+      state: { data }
     });
   };
 
-  const handleViewSelectedCourses = () => {
-    const selectedCoursesString = JSON.stringify(selectedCourses);
-    history.push(`/addCourse?selectedCourses=${selectedCoursesString}`);
-  };
-
   return (
-    <div>
-      <ParentComponent/>
-      <div className='course-tabl'>
-      <h1>Courses Page</h1>
-      <CourseTable courses={courses} handleCheckboxChange={handleCheckboxChange} />
-      {selectedCourses.length > 0 && (
-        <button onClick={handleViewSelectedCourses}>View Selected Courses</button>
-      )}
+    <div className="course-page">
+   <ParentComponent/>
+      <div className="course-content">
+        <div className="course-list-container">
+          <h2>Courses</h2>
+          <CourseList courses={courses} handleCourseClick={handleCourseClick} handleCheckboxChange={handleCheckboxChange} />
+        </div>
+        <div className="sub-course-list-container">
+          
+                  <button className='selected_course' onClick={handleSubCourseClick}>Add Selected Courses</button>
+        </div>
       </div>
     </div>
   );
 };
 
-export default CoursesPage;
+export default CoursePage;
--- src/Student_centre/addCourses.js
+++ src/Student_centre/addCourses.js
@@ -1,49 +1,25 @@
-import React, { useState } from 'react';
+// AddCoursePage.js
+import React from 'react';
 import { useLocation } from 'react-router-dom';
-import ParentComponent from './admin_nav';
-import { FaTrash } from 'react-icons/fa';
-import '../Student_centre/admin_css/addCourse.css';
 
-const AddDropPage = () => {
+const AddCoursePage = () => {
   const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const selectedCoursesString = searchParams.get('selectedCourses');
-  const [selectedCourses, setSelectedCourses] = useState(selectedCoursesString ? JSON.parse(selectedCoursesString) : []);
-
-  const handleRemoveCourse = (index) => {
-    const updatedCourses = [...selectedCourses];
-    updatedCourses.splice(index, 1);
-    setSelectedCourses(updatedCourses);
-  };
+  const { selectedCourse, selectedSubCourses } = location.state.data;
 
   return (
     <div>
-      <h1 className='selectedCourse'>Selected Courses</h1>
-      <ParentComponent />
-      <div className='vo'>
-        <table className="course-table">
-          <thead>
-            <tr>
-              <th className="course-header">Course</th>
-              <th className="credit-points">Credit Points</th>
-              <th className="select-checkbox">Select</th>
-            </tr>
-          </thead>
-          <tbody>
-            {selectedCourses.map((course, index) => (
-              <tr key={index}>
-                <td>{course.name}</td>
-                <td>{course.creditPoints}</td>
-                <td onClick={() => handleRemoveCourse(index)}>
-                  <FaTrash />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <h1>Add Course Page</h1>
+      <div>
+        <h2>Selected Course: {selectedCourse}</h2>
+        <h2>Selected Sub-Courses:</h2>
+        <ul>
+          {selectedSubCourses.map(subCourse => (
+            <li key={subCourse.id}>{subCourse.name}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
 };
 
-export default AddDropPage;
+export default AddCoursePage;
