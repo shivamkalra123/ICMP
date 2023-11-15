d2a70d1de0d529ac6d44c362d53d9da7e7140386
Shivam <kalrashivam47@gmail.com>
2023-11-15 06:05:20 +0530

    faq-changes

--- src/components/faq.js
+++ src/components/faq.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 import { FaChevronDown } from 'react-icons/fa';
 import '../comp_css/faq.css';
 import Navbar from './nav';
@@ -6,6 +7,12 @@ import Footer from './footer';
 
 const Faqs = () => {
   const [open, setOpen] = useState(null);
+
+  // React Spring animation config
+  const contentSpring = useSpring({
+    opacity: open === null ? 1 : 0.2,
+    maxHeight: open === null ? 1000 : 200, // Set your desired max height
+  });
 
   const toggleFAQ = (index) => {
     setOpen(open === index ? null : index);
@@ -15,21 +22,22 @@ const Faqs = () => {
     <div>
       <div className="wrapper1">
         <Navbar />
-        <h1 className='main-head'>ICMP FAQ</h1>
+        <h1 className="main-head">ICMP FAQ</h1>
         <div>
           <details open={open === 0} onClick={() => toggleFAQ(0)}>
             <summary>
               How do you create an accordion?
             </summary>
-            <div>
+            {/* Wrap the content in animated.div from React Spring */}
+            <animated.div style={contentSpring}>
               Easy! As long as you don't have to support IE11 or older browsers you could use <code>&lt;details&gt;</code> and <code>&lt;summary&gt;</code> natively.
-            </div>
+            </animated.div>
           </details>
           {/* Add more details elements with summaries and content */}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Faqs;
